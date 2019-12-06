f81f2af3fe63edc7e53b1dec98c5c6a919542388
uralskdev <58599864+uralskdev@users.noreply.github.com>
2019-12-06 19:52:46 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var (
+	indexhtml string
+)
+
+func main() {
+	indexfile, readerror := ioutil.ReadFile("index.html")
+	if readerror != nil {
+		log.Fatal("ReadFile ", readerror)
+	}
+	indexhtml = string(indexfile)
+	go http.HandleFunc("/", index)
+	err := http.ListenAndServe(":9000", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		request.ParseForm()
+		fmt.Println(request.Form)
+	}
+	fmt.Fprintf(writer, indexhtml)
+}
 
